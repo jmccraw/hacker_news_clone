da7490e3ae87adc26b26cbbfad391f489194b827
Justin McCraw <jmccraw1@gmail.com>
2021-02-20 19:40:02 -0500

    Adjust Comments logic to use meta info component and dangerously set innerHTML

--- app/components/Comments.js
+++ app/components/Comments.js
@@ -1,20 +1,113 @@
 import React from 'react';
 import queryString from 'query-string';
+import { ThemeConsumer } from '../contexts/theme';
+import { fetchComments, fetchItem } from '../utils/api';
+import Loading from './Loading';
+import { PostListItem, PostMetaInfo } from './PostsList';
 
-export default class Post extends React.Component {
+function Comment({ by, id, text, time, ...restProps }) {
+  return (
+    <div className="comment">
+      <PostMetaInfo
+        by={by}
+        time={time}
+      />
+      <p dangerouslySetInnerHTML={{__html: text}} />
+    </div>
+  );
+}
+
+export default class Comments extends React.Component {
   state = {
-    id: ''
+    comments: {},
+    error: null,
+    loading: false
   };
 
   componentDidMount() {
+    const { id } = queryString.parse( this.props.location.search );
 
+    fetchItem( id )
+      .then( data => {
+        window.console.log( 'POST', data );
+
+        this.setState({
+          comments: {
+            ...data
+          },
+          error: null,
+          loading: false
+        });
+
+        this.getPostComments();
+      } )
+      .catch( ({ message }) => {
+        this.setState({
+          error: message,
+          loading: false
+        });
+      } );
+  }
+
+  /**
+   * Get the comments related to this post
+   */
+  getPostComments() {
+    fetchComments( this.state.comments.kids )
+      .then( postComments => {
+        window.console.log( 'COMMENTS: ', postComments );
+
+        this.setState(({ comments }) => ({
+          comments: {
+            ...comments,
+            posts: postComments
+          }
+        }));
+      } )
+      .catch( ({ message }) => {
+        this.setState({
+          error: message,
+          loading: false
+        });
+      });
   }
 
   render() {
+    const { comments, error, loading } = this.state;
+    const { by, id, time, kids, title, url, posts } = comments;
+
     return (
-      <article className="post">
-        <h1>Post</h1>
-      </article>
+      <>
+        {loading && <Loading text="Fetching post" />}
+
+        {error && <p className="error-message">{error}</p>}
+        <ThemeConsumer>
+          {({ theme }) => (
+            <>
+              {! loading && title && <>
+                <PostListItem
+                  title={title}
+                  url={url}
+                  header={true}
+                />
+                <PostMetaInfo
+                  by={by}
+                  time={time}
+                  id={id}
+                  kids={kids}
+                />
+              </>}
+
+              {posts && posts.map( post => (
+                <Comment
+                  key={post.id}
+                  {...post}
+                />
+              ))}
+            </>
+          )}
+        </ThemeConsumer>
+      </>
     );
   }
 }
